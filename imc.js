9e6103fd46cac9fe3b4ae2fac52cb2c713ee269b
santosoliviera <jsfolivier@gmail.com>
2024-11-04 11:34:26 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,20 +1,20 @@
 function imc(){
-    const formulario = document.getElementsByName("formulario")[0]
-    const formDados = new FormData(formulario)
+    const formulário = document.getElementsByName("formulario")[0]
+    const formados = new FormData(formulario)
     const paciente = {
-        nome:formDados.get("nome"),
-        email:formDados.get("email"),
-        dataNasc:formDados.get("nasc"),
-        telefone:formDados.get("tel"),
+        nome:formados.get("nome"),
+        email:formados.get("email"),
+        dataNasc:formados.get("nasc"),
+        telefone:formados.get("tel"),
         sexo:document.querySelector('input[name=sexo]:checked').value,
-        peso:formDados.get("peso"),
-        altura:formDados.get("altura"),
+        peso:formados.get("peso"),
+        altura:formados.get("altura"),
         classificacao:"",
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade mÃ³rbida";
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudÃ¡vel"
+            if (imc < 18.5 ) this.classificacao = "obesidade mórbida";
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudável"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
@@ -29,5 +29,5 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificaÃ§Ã£o de peso Ã© " + paciente.classificacao;
+    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificaçãoo de peso Ã© " + paciente.classificacao;
 }
